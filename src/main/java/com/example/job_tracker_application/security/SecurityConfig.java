7984075bd31b48103d7b5e55e02f0c0153d846e2
hiteshSingh57 <hiteshsingh001361@gmail.com>
2026-06-14 15:57:43 +0530

    feat: add swagger documantation with JWT authentication

--- src/main/java/com/example/job_tracker_application/security/SecurityConfig.java
+++ src/main/java/com/example/job_tracker_application/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
